ed95c870b4a33664ea698d4a993303b30478e91f
WeiJun Syu <weijunsyu@gmail.com>
2020-07-03 16:51:47 -0700

    Camera settings
    added a camera oject script to adjust aspect ratio and fix it to a constant value (useful if in windowed mode, etc where we want to preserve some ratio regardless of window size)

---
+++ MapleHunter2D/Assets/Scripts/CameraLogic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CameraLogic : MonoBehaviour
+{
+
+    private void Start()
+    {   // Adjust the screen size such that no matter what the physical screen size the game size will always be constant
+        AdjustCameraAspectRatio(GameConstants.TARGET_SCREEN_WIDTH_BY_RATIO, GameConstants.TARGET_SCREEN_HEIGHT_BY_RATIO);
+    }
+
+    //could be moved to something global? and not some object? debatable
+    private void AdjustCameraAspectRatio(float targetWidthByRatio, float targetHeightByRatio)
+    {
+        float targetAspect = targetWidthByRatio / targetHeightByRatio;
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+        float scaleDifference = windowAspect / targetAspect;
+        UnityEngine.Camera.main.aspect = targetAspect;
+
+        if (scaleDifference < 1.0f) //add letterboxes (horizontal lines on the top and bottom)
+        {
+            float cameraHeight = UnityEngine.Camera.main.orthographicSize * 2;
+            float cameraWidth = targetAspect * cameraHeight;
+            float offset = ((cameraWidth * Screen.height) - (Screen.width * cameraHeight)) / (Screen.width * 2);
+            UnityEngine.Camera.main.aspect = windowAspect;
+            UnityEngine.Camera.main.orthographicSize += offset;
+        }
+        else //add pillarboxes (vertical lines on the left and right)
+        {
+            UnityEngine.Camera.main.aspect = windowAspect;
+        }
+    }
+}
--- MapleHunter2D/Assets/Scripts/GameConstants.cs
+++ MapleHunter2D/Assets/Scripts/GameConstants.cs
@@ -1,5 +1,13 @@
 ﻿
+using System;
+
 public static class GameConstants
 {
+    //save data
     public const string SAVEPATH = "/savedata.save";
+
+    //camera settings
+    public const float TARGET_SCREEN_WIDTH_BY_RATIO = 16f; //the 16 part of 16:9 aspect ratio
+    public const float TARGET_SCREEN_HEIGHT_BY_RATIO = 9f; //the 9 part of 16:9 aspect ratio
+
 }
